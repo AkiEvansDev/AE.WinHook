7c70127e623eae80b10eb59d42a4f5bee48cae95
Aki Evans <aki.evans.dev@gmail.com>
2023-05-14 12:54:17 +0300

    Add project files.

---
+++ Hook/MouseHook.cs
@@ -0,0 +1,64 @@
+﻿using System.Runtime.InteropServices;
+
+namespace AE.WinHook.Hook;
+
+public enum MouseEventType
+{
+    None = 0,
+    MouseDown = 1,
+    MouseUp = 2,
+    MouseMove = 3,
+    MouseWheel = 4,
+    DoubleClick = 5,
+}
+
+public enum MouseButtonType
+{
+    None = 0,
+    Left = 1,
+    Right = 2,
+    Middle = 3,
+}
+
+public delegate bool OnHookMouse(int x, int y, MouseButtonType buttonType, MouseEventType eventType);
+
+public class MouseHook : BaseHook
+{
+    public OnHookMouse MouseEvent { get; set; }
+
+    public MouseHook()
+    {
+        HookType = WH_MOUSE_LL;
+    }
+
+    protected override int HookCallbackProcedure(int nCode, int wParam, IntPtr lParam)
+    {
+        if (nCode > -1 && MouseEvent != null)
+        {
+            var mouseHookStruct = (MouseLLHookStruct)Marshal.PtrToStructure(lParam, typeof(MouseLLHookStruct));
+
+            var mouseButton = wParam switch
+            {
+                WM_LBUTTONDOWN or WM_LBUTTONUP or WM_LBUTTONDBLCLK => MouseButtonType.Left,
+                WM_RBUTTONDOWN or WM_RBUTTONUP or WM_RBUTTONDBLCLK => MouseButtonType.Right,
+                WM_MBUTTONDOWN or WM_MBUTTONUP or WM_MBUTTONDBLCLK => MouseButtonType.Middle,
+                _ => MouseButtonType.None,
+            };
+
+            var eventType = wParam switch
+            {
+                WM_LBUTTONDOWN or WM_RBUTTONDOWN or WM_MBUTTONDOWN => MouseEventType.MouseDown,
+                WM_LBUTTONUP or WM_RBUTTONUP or WM_MBUTTONUP => MouseEventType.MouseUp,
+                WM_MOUSEMOVE => MouseEventType.MouseMove,
+                WM_MOUSEWHEEL => MouseEventType.MouseWheel,
+                WM_LBUTTONDBLCLK or WM_RBUTTONDBLCLK or WM_MBUTTONDBLCLK => MouseEventType.DoubleClick,
+                _ => MouseEventType.None,
+            };
+
+            if (eventType != MouseEventType.None && MouseEvent(mouseHookStruct.pt.x, mouseHookStruct.pt.y, mouseButton, eventType))
+                return 1;
+        }
+
+        return CallNextHookEx(HandleToHook, nCode, wParam, lParam);
+    }
+}
---
+++ HotKeyRegister.cs
@@ -0,0 +1,159 @@
+﻿using System.Windows.Input;
+
+using AE.WinHook.Hook;
+
+namespace AE.WinHook;
+
+public static class HotKeyRegister
+{
+    private static readonly KeyboardHook KeyboardHook = new();
+
+    static HotKeyRegister()
+    {
+        KeyboardHook.KeyDown = OnHookKeyDown;
+        KeyboardHook.KeyUp = OnHookKeyUp;
+    }
+
+    private static readonly List<Key> PressKeys = new();
+
+    private static bool OnHookKeyDown(KeyModifiers keyModifiers, Key key)
+    {
+        if (!PressKeys.Contains(key))
+        {
+            foreach (var pressKey in PressKeys.ToList())
+            {
+                if (!KeyboardHook.IsKeyDown(pressKey))
+                    PressKeys.Remove(pressKey);
+            }
+
+            if (KeyboardHook.IsKeyModifiers(key))
+                keyModifiers = KeyboardHook.GetKeyModifiers(key);
+            else
+                PressKeys.Add(key);
+
+            return Invoke(keyModifiers);
+        }
+
+        return false;
+    }
+
+    private static bool OnHookKeyUp(KeyModifiers keyModifiers, Key key)
+    {
+        if (PressKeys.Contains(key))
+            PressKeys.Remove(key);
+
+        return false;
+    }
+
+    private class HotKey
+    {
+        public KeyModifiers Modifiers { get; set; }
+        public string Keys { get; set; }
+        public Action Action { get; set; }
+        public bool Handled { get; set; }
+
+        public override string ToString()
+        {
+            return
+                $"{(Modifiers.HasFlag(KeyModifiers.Control) ? "Ctrl + " : "")}" +
+                $"{(Modifiers.HasFlag(KeyModifiers.Shift) ? "Shift + " : "")}" +
+                $"{(Modifiers.HasFlag(KeyModifiers.Alt) ? "Alt + " : "")}" +
+                $"{(Modifiers.HasFlag(KeyModifiers.Win) ? "Win + " : "")}" +
+                $"{Keys.Replace("+", " + ")}"
+                .TrimEnd();
+        }
+    }
+
+    private static readonly List<HotKey> HotKeys = new();
+
+    private static bool Invoke(KeyModifiers keyModifiers)
+    {
+        var result = false;
+        var keysString = string.Join('+', PressKeys);
+
+        foreach (var hotKey in HotKeys.Where(hk => hk.Modifiers == keyModifiers))
+            if (hotKey.Keys == keysString)
+            {
+                hotKey.Action?.Invoke();
+                result = hotKey.Handled || result;
+            }
+
+        return result;
+    }
+
+    public static bool RegHotKey(KeyModifiers keyModifiers, Key key, Action action, bool handled = true)
+    {
+        return RegHotKey(keyModifiers, new List<Key> { key }, action, handled);
+    }
+
+    public static bool RegHotKey(KeyModifiers keyModifiers, IEnumerable<Key> keys, Action action, bool handled = true)
+    {
+        var hotKey = new HotKey
+        {
+            Modifiers = keyModifiers,
+            Keys = string.Join('+', keys),
+            Action = action,
+            Handled = handled,
+        };
+
+        var duplicateHotKey = HotKeys.FirstOrDefault(hk => hk.Modifiers == hotKey.Modifiers && hk.Keys == hotKey.Keys);
+        if (duplicateHotKey != null)
+        {
+            duplicateHotKey.Action = action;
+            duplicateHotKey.Handled = handled;
+        }
+        else
+        {
+            duplicateHotKey = HotKeys.FirstOrDefault(hk => hk.Modifiers == hotKey.Modifiers
+                && hk.Keys != hotKey.Keys
+                && (hk.Keys.StartsWith(hotKey.Keys) || hotKey.Keys.StartsWith(hk.Keys))
+            );
+
+            if (duplicateHotKey != null)
+                throw new Exception($"Hot key `{hotKey}` coincides with `{duplicateHotKey}`!");
+
+            HotKeys.Add(hotKey);
+
+            if (!KeyboardHook.IsStarted)
+                KeyboardHook.Start();
+        }
+
+        return true;
+    }
+
+    public static bool UnregHotKey(KeyModifiers keyModifiers, Key key)
+    {
+        return UnregHotKey(keyModifiers, new List<Key> { key });
+    }
+
+    public static bool UnregHotKey(KeyModifiers keyModifiers, IEnumerable<Key> keys)
+    {
+        var hotKey = new HotKey
+        {
+            Modifiers = keyModifiers,
+            Keys = string.Join('+', keys),
+        };
+
+        var duplicateHotKey = HotKeys.FirstOrDefault(hk => hk.Modifiers == hotKey.Modifiers && hk.Keys == hotKey.Keys);
+        if (duplicateHotKey != null)
+        {
+            HotKeys.Remove(duplicateHotKey);
+
+            if (!HotKeys.Any())
+                KeyboardHook.Stop();
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public static bool UnregAllHotKey()
+    {
+        HotKeys.Clear();
+        KeyboardHook.Stop();
+
+        return true;
+    }
+}
+
